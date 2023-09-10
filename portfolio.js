134462b8ba9482758d7d0af5798bf2527662c330
Beutife <biholuwatisefeh@gmail.com>
2023-09-10 18:44:28 +1300

    current day and time in seconds

--- portfolio.js
+++ portfolio.js
@@ -1,8 +1,8 @@
 // JavaScript to update the counter every second
 function updateUTCTime() {
     const currentTime = new Date();
-    const utcTime = currentTime.toUTCString();
-    document.getElementById("time").textContent = utcTime;
+    const secondsTime = Math.round(currentTime.getTime() / 1000); // Convert milliseconds to seconds
+    document.getElementById("time").textContent = secondsTime;
 
     console.log(currentTime); // Remove quotes around 'currentTime'
 
@@ -12,3 +12,15 @@ function updateUTCTime() {
 
 // Call the updateUTCTime function to initially display the time
 updateUTCTime();
+
+function updateDay() {
+    const currentTime = new Date();
+    const dayOfWeek = ["Sunday", "Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+    const currentDay = dayOfWeek[currentTime.getUTCDay()];
+    document.getElementById("day").textContent = currentDay;
+
+    console.log(currentDay);
+
+    setTimeout(updateDay,1000);
+}
+updateDay();
